c38ed1ec3140a5a15df5f28b6bf504ab2d0eec0e
Artem Soldatov <artemsoldatov1@gmail.com>
2017-01-10 18:14:27 +0200

    third commit

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<title>Document</title>
 </head>
 <body>
-	<p>Title</p>
+	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores maxime nobis ab explicabo magnam. Aperiam veritatis dolor quia, at nemo! Illum nesciunt minima ipsum, debitis eligendi esse sequi a aliquam.</p>
+
 </body>
 </html>
